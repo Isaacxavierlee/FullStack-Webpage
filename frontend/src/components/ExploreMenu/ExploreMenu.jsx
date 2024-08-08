45ee8ee507b6e9ea7919edb436384eba139a47ff
Isaacxavierlee <reivax.caasi@gmail.com>
2024-08-08 14:16:45 +0800

    button

--- frontend/src/components/ExploreMenu/ExploreMenu.jsx
+++ frontend/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,31 +1,33 @@
-import React from 'react'
-import './ExploreMenu.css'
-import { menu_list } from '../../assets/assets'
+import React from 'react';
+import './ExploreMenu.css';
+import { menu_list } from '../../assets/assets';
 
-const ExploreMenu = () => {
+const ExploreMenu = ({ category, setCategory }) => {
   return (
     <div className='explore-menu' id='explore-menu'>
-        <h1>Explore our Menu</h1>
-        <p className='explore-menu'> 
-            Choose from a variety of meet , freshly flown from Australia straight to your table Daily!
+        <h1>Explore Our Menu</h1>
+        <p>
+            Choose from a variety of meats, freshly flown from Australia straight to your table daily!
+        </p>
         <div className='explore-menu-list'>
-            {menu_list.map((item,index)=>{
-                return (
-                    <div key={index} className='explore-menu-list-item'>
-                    <img src={item.menu_image} alt='' />
+            {menu_list.map((item, index) => (
+                <div
+                    key={index}
+                    onClick={() => setCategory(prev => prev === item.menu_name ? "All" : item.menu_name)}
+                    className='explore-menu-list-item'
+                >
+                    <img
+                        className={category === item.menu_name ? "active" : ""}
+                        src={item.menu_image}
+                        alt={item.menu_name}
+                    />
                     <p>{item.menu_name}</p>
                 </div>
-                )
-
-            })}
-
+            ))}
         </div>
-        
-        
-        </p>
-      
+        <hr />
     </div>
-  )
+  );
 }
 
-export default ExploreMenu
+export default ExploreMenu;
